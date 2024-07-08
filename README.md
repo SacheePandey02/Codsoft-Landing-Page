de21683006d9e4d8f78800cc21f1c5a0ee62ff79
Sachee Pandey <you@example.com>
2024-07-08 10:05:38 +0530

    rename of images

---
+++ README.md
@@ -0,0 +1,2 @@
+# Codsoft-Landing-Page
+This is responsive page of travelling website named as Raahi . Made it for task 2 of Codsoft
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const scrollRevealOption ={
     distance: "50px",
     origin: "bottom",
@@ -65,3 +66,72 @@ ScrollReveal().reveal(".journals_card",{
 
 
 
+=======
+const scrollRevealOption ={
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+//header container
+ScrollReveal().reveal(".header_container h1" ,scrollRevealOption);
+
+ScrollReveal().reveal(".header_container h4",{
+    ...scrollRevealOption,
+    delay:500,
+
+});
+
+ScrollReveal().reveal(".header_container .btn",{
+    ...scrollRevealOption,
+    delay:1000,
+
+});
+
+  //about container
+
+  ScrollReveal().reveal(".about_container .section_header", scrollRevealOption);
+  ScrollReveal().reveal(".about_container .section_subheader",{
+    ...scrollRevealOption,
+    delay: 500,
+  });
+  
+  ScrollReveal().reveal(".about_container .about_flex",{
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+
+  ScrollReveal().reveal(".about_container .btn",{
+    ...scrollRevealOption,
+    delay: 1500,
+  });
+
+  //Discover container
+  
+  ScrollReveal().reveal(".discover_card",{
+    ...scrollRevealOption,
+    interval: 500,
+  });
+
+  
+  ScrollReveal().reveal(".discover_card_content",{
+    ...scrollRevealOption,
+    interval:500,
+    delay: 200,
+  });
+
+  
+//Blogs Container
+ScrollReveal().reveal(".blogs_card",{
+    duration:1000,
+    interval:400,
+});
+
+//journals container
+ScrollReveal().reveal(".journals_card",{
+    interval:400,
+});
+  
+
+
+
+>>>>>>> 10ba24bdd9a6f0218eba9229adf87476bde918ca
